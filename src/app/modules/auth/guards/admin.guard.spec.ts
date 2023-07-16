a3257065416443b4a143542629f47592851a519c
alxael <alex.aelenei04@gmail.com>
2023-07-16 12:32:53 +0300

    Added minor fixes.

--- src/app/modules/auth/guards/admin.guard.spec.ts
+++ src/app/modules/auth/guards/admin.guard.spec.ts
@@ -1,17 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
 
-import { adminGuard } from './admin.guard';
+import { AdminGuard } from './admin.guard';
 
-describe('adminGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => adminGuard(...guardParameters));
+describe('AdminGuard', () => {
+  let guard: AdminGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    guard = TestBed.inject(AdminGuard);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
   });
 });
